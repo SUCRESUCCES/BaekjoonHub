5a0b172a607216d62f49ed512baa0ca3d7158677
sucresucces <sucresucces@icloud.com>
2025-08-10 23:34:16 +0900

    [level 0] Title: 문자열 돌리기, Time: 63.36 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    let strArr = str.split('');
+    strArr.forEach(function(result){
+        console.log(result);
+        
+});
+});
