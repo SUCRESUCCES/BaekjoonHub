d2b870ee9b4b6f52932c8b126b59be3fd19ba201
sucresucces <sucresucces@icloud.com>
2025-09-01 13:26:59 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    
+    
+    const odd = Number(num_list.filter(num => num % 2 !== 0).join(""));
+    const even = Number(num_list.filter(num => num % 2 === 0).join(""));
+    
+    return odd + even
+           
+}
