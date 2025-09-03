1e410a044f1470f234d53c60695b775396f1f1ad
sucresucces <sucresucces@icloud.com>
2025-09-03 15:17:30 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.18 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,20 @@
+function solution(n, control) {
+    let answer = n;
+    for(const char of control){
+        switch(char){
+            case "w" : 
+                answer += 1;
+                break;
+            case "s" :
+                answer -= 1;
+                break;
+            case "d" :
+                answer += 10;
+                break;
+            case "a" :
+                answer -= 10;
+                break;
+        }
+    }
+    return answer;
+}
