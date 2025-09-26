dd47a3ce1bf85da6dce9ae82d7cf850f66c02497
sucresucces <sucresucces@icloud.com>
2025-09-26 11:23:45 +0900

    [level 0] Title: qr code, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,9 +1,3 @@
 function solution(q, r, code) {
-    var answer = '';
-    for(let i = 0; i < code.length; i++){
-        if(i % q === r){
-           answer += code[i]; 
-        }
-    }    
-    return answer;
+    return [...code].filter((ch, i)=> i % q === r).join(''); 
 }
