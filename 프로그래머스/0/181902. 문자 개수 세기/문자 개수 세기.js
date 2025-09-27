cfe670c83b2b4d3b541f1e71c1748c3c2b4748e4
sucresucces <sucresucces@icloud.com>
2025-09-27 13:36:52 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,13 +1,15 @@
 function solution(my_string) {
-    var answer = Array(52).fill(0);
-    
-    [...my_string].forEach(ch => {
-        let code = ch.charCodeAt(0);
-        if(code >= 65 && code <= 90){
-            answer[code - 65]++;
-        }else if(code >= 97 && code <= 122){
-            answer[26 + (code - 97)]++;
+    return [...my_string].reduce((acc, ch) => { 
+        // 문자열을 스프레드(...)로 풀어 문자 배열 만든 뒤, reduce로 순회
+        // acc : 누적 배열(길이 52)
+        // ch : 현재 순회 중인 문자
+        
+        let code = ch.charCodeAt(0);    // 현재 문자의 유니코드 값
+        if(code >= 65 && code <= 90){   // 대문자 : A(65) ~ Z(90)
+            acc[code - 65]++;           // A는 인덱스 0, Z는 인덱스 25
+        }else if(code >= 97 && code <= 122){    // 소문자 : a(97) ~ z(122)
+            acc[26 + (code - 97)]++;            // a는 인덱스 26, z는 인덱스 51
         }
-    });
-    return answer;
+        return acc; // 수정된 누적 배열 반환 -> 다음 reduce 단계로 전달
+    },Array(52).fill(0));   // 초기값 : [0, 0, ...0](길이 52)
 }
