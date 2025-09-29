75b0bc851d4f321476c0e65a69be2c7605ec1951
sucresucces <sucresucces@icloud.com>
2025-09-29 13:52:08 +0900

    [level 0] Title: 글자 지우기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,5 +1,3 @@
 function solution(my_string, indices) {
-    let chars = my_string.split('');
-    const result = chars.filter((_, i) => !indices.includes(i)).join('');
-    return result;
+    return [...my_string].filter((_, i) => !indices.includes(i)).join("");
 }
