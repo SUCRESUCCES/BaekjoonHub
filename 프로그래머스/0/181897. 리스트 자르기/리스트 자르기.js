7fea533c86d0ab43db9f30818bcb5f66d2bc3d85
sucresucces <sucresucces@icloud.com>
2025-10-02 15:21:36 +0900

    [level 0] Title: 리스트 자르기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,8 +1,14 @@
 function solution(n, slicer, num_list) {
-    const [a, b, c] = slicer;
-    
-    if(n === 1) return num_list.filter((_, i) => i <= b);
-    if(n === 2) return num_list.filter((_, i) => i >= a);
-    if(n === 3) return num_list.filter((_, i) => i >= a && i <= b);
-    if(n === 4) return num_list.filter((_, i) => i >= a && i <= b && (i-a) % c === 0);
+    let [a, b, c] = [...slicer];
+
+    switch(n) {
+        case 1:
+            return num_list.slice(0, b + 1);
+        case 2:
+            return num_list.slice(a);
+        case 3:
+            return num_list.slice(a, b + 1);
+        case 4:
+            return num_list.slice(a, b + 1).filter((_, idx) => !(idx % c));
+    }
 }
