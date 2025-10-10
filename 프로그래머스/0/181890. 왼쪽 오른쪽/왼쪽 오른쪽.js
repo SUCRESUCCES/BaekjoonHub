50d562f608121dd0495fbebc4e455a37d5e1d985
sucresucces <sucresucces@icloud.com>
2025-10-10 13:41:48 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,8 @@
+function solution(str_list) {
+    let l = [...str_list].indexOf("l");
+    let r = [...str_list].indexOf("r");
+    
+    if(l === -1 && r === -1) return [];
+    if(l !== -1 && (r === -1 || l < r)) return [...str_list].slice(0, l);
+    if(r !== -1 && (l === -1 || r < l)) return [...str_list].slice(r+1);
+}
